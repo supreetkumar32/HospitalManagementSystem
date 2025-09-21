355540d88e1243740128be5344416ee3d871e8da
Supreet <supreetkumar32@gmail.com>
2025-09-21 18:57:25 +0530

    initial commit 3.5

---
+++ src/main/java/com/three/projections/projections/entity/Patient.java
@@ -0,0 +1,102 @@
+package com.three.projections.projections.entity;
+
+import com.three.projections.projections.entity.type.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private String email;
+
+    private String gender;
+
+    @Enumerated(value=EnumType.STRING)
+    private BloodGroupType bloodGroup;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public BloodGroupType getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setBloodGroup(BloodGroupType bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", bloodGroup=" + bloodGroup +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
